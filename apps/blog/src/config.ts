0fc8d28ef61f1e9fe88a4292f01f1a730a22bb5a
STTTOS <744765302@qq.com>
2024-04-15 16:52:33 +0800

    fix: 修复鉴权控制

--- apps/blog/src/config.ts
+++ apps/blog/src/config.ts
@@ -43,5 +43,5 @@ export const apiNeededToAuth = [
   '/api/common/upload',
   '/api/common/upload_file',
   '/api/common//upload_temp_file',
-  '/api/common//deploy_blog_frontend'
+  '/api/common/deploy_blog_frontend'
 ]
