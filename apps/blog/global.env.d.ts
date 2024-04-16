f5e2afb7d2c3276ac5b63fe4cd440da599e9a30a
STTTOS <744765302@qq.com>
2024-04-16 14:43:24 +0800

    feat: 自动生成env.d.ts声明文件

--- apps/blog/global.env.d.ts
+++ apps/blog/global.env.d.ts
@@ -1,13 +1,8 @@
-// global-env.d.ts
 declare namespace NodeJS {
   interface ProcessEnv {
-    /**数据库链接url */
     DATABASE_URL: string
-    /**token加密米哦啊 */
     SECRET_KEY: string
-    /**腾讯cos秘钥key */
     COS_SECRET_KEY: string
-    /**腾讯cos秘钥id */
     COS_SECRET_ID: string
   }
 }
--- apps/blog/src/scripts/initSystem.ts
+++ apps/blog/src/scripts/initSystem.ts
@@ -1,26 +1,70 @@
 import fs from 'fs'
 import { join } from 'path'
-import { access } from 'fs/promises'
+import { access, readFile, writeFile } from 'fs/promises'
+import { map, trim, head, split, filter, compose } from 'ramda'
 
 import { logger } from '../logger'
 
-async function createFiles() {
-  await access(join(__dirname, '../../public')).catch(() => {
-    fs.mkdirSync(join(__dirname, '../../public'))
-  })
-  await access(join(__dirname, './static')).catch(() => {
-    fs.mkdirSync(join(__dirname, '../../static/origin'), { recursive: true })
-    fs.mkdirSync(join(__dirname, '../../static/files'), { recursive: true })
-    fs.mkdirSync(join(__dirname, '../../static/temp'), { recursive: true })
-  })
-  await access(join(__dirname, '../system.json')).catch(() => {
-    fs.writeFileSync(join(__dirname, '../system.json'), '{ "viewCount": 0 }', {
-      encoding: 'utf-8'
-    })
-  })
+async function initSystem() {
+  logger.info('初始化系统中...')
+  await Promise.allSettled([
+    access(join(__dirname, '../../public')).catch(() => {
+      fs.mkdirSync(join(__dirname, '../../public'))
+    }),
+    access(join(__dirname, './static')).catch(() => {
+      fs.mkdirSync(join(__dirname, '../../static/origin'), { recursive: true })
+      fs.mkdirSync(join(__dirname, '../../static/files'), { recursive: true })
+      fs.mkdirSync(join(__dirname, '../../static/temp'), { recursive: true })
+    }),
+    access(join(__dirname, '../system.json')).catch(() => {
+      fs.writeFileSync(
+        join(__dirname, '../system.json'),
+        '{ "viewCount": 0 }',
+        {
+          encoding: 'utf-8'
+        }
+      )
+    }),
+    createEnvDeclaration()
+  ])
+  logger.info('初始化系统完成')
+}
+/**
+ * @description 从.env环境中生成.d.ts声明文件
+ */
+async function createEnvDeclaration() {
+  const dotEnvPath = join(__dirname, '../../.env')
+  try {
+    await access(dotEnvPath)
+    const envsSource = await readFile(dotEnvPath, { encoding: 'utf-8' })
+
+    const filterEmptyAndAnnotation = (input: string) =>
+      !!input && !input.trimStart().startsWith('#')
+
+    const getEnvKeys = compose(
+      map(compose(trim, head, split('='))),
+      filter(filterEmptyAndAnnotation),
+      split('\n')
+    )
+    const keys = getEnvKeys(envsSource)
+
+    const interfaceContent = keys
+      .map((key) => {
+        return `${key}: string`
+      })
+      .join('\n\t\t')
+    await writeFile(
+      join(__dirname, '../../global.env.d.ts'),
+      `declare namespace NodeJS {
+  interface ProcessEnv {
+    ${interfaceContent}
+  }
+}
+`
+    )
+  } catch (error) {
+    logger.error((error as Error).message)
+  }
 }
 
-logger.info('初始化系统中...')
-createFiles().then(() => {
-  logger.info('初始化系统完成')
-})
+initSystem()
