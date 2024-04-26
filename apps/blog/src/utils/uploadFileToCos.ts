036e7d4288250a142d29879a6a53460964ad1410
STTTOS <744765302@qq.com>
2024-04-26 18:54:01 +0800

    fix: 修改上传cos路径

--- apps/blog/src/utils/uploadFileToCos.ts
+++ apps/blog/src/utils/uploadFileToCos.ts
@@ -1,3 +1,4 @@
+import { join } from 'path'
 import Cos from 'cos-nodejs-sdk-v5'
 
 const cos = new Cos({
@@ -8,7 +9,7 @@ const cos = new Cos({
 })
 
 const uploadFileToCos = (
-  appName: string,
+  prefix: string,
   fileName: string,
   filePath: string
 ) => {
@@ -22,7 +23,7 @@ const uploadFileToCos = (
         /* 存储桶所在地域，例如ap-beijing，必须字段 */
         Region: 'ap-chengdu',
         /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段 */
-        Key: fileName,
+        Key: join(prefix, fileName),
         /* 必须，上传文件对象，可以是input[type="file"]标签选择本地文件后得到的file对象 */
         FilePath: filePath,
         /* 触发分块上传的阈值，超过5MB使用分块上传，非必须 */
