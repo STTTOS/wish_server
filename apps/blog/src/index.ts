979c73ab3958b5ce87a1294b25ec8e55e75fc1b4
STTTOS <744765302@qq.com>
2024-05-13 16:08:03 +0800

    fix: 修改@路径

--- apps/blog/src/index.ts
+++ apps/blog/src/index.ts
@@ -1,9 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const moduleAlias = require('module-alias')
-const path = require('path')
 // Or multiple aliases
 moduleAlias.addAliases({
-  '@': path.join(__dirname, '../src')
+  '@': __dirname
 })
 require('./start')
