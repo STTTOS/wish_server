d5640681398450fda345af7f98012b8e54f2b3ef
yangchengrong <yangchnegrong@yzw.cn>
2025-02-18 13:54:42 +0800

    Revert "fix: 访问/下的资源使用默认缓存策略"
    This reverts commit 3e2728c345445f615e963829ddfcb1b65b76f4ee.

--- apps/blog/src/start.ts
+++ apps/blog/src/start.ts
@@ -37,7 +37,7 @@ app.use(
   })
 )
 // 访问 网站静态文件
-app.use(mount('/', serve(join(__dirname, '../public'))))
+app.use(mount('/', serve(join(__dirname, '../public'), { maxAge })))
 
 // 访问 网站静态文件
 app.use(mount('/public', serve(join(__dirname, '../public'), { maxAge })))
