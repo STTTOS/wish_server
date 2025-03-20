9f42624c87ecf05fcfc8b09bad9afe33aecea1d2
yangchengrong <yangchnegrong@yzw.cn>
2025-03-20 16:55:32 +0800

    fix: bugfix

--- apps/texas/src/middleware/customHandle401.ts
+++ apps/texas/src/middleware/customHandle401.ts
@@ -7,7 +7,7 @@ import { DefaultState } from '../router/instance'
 const loginUsers = new Map<number, { sessionId: string; time: string }>()
 // 如下的接口, 即使解析不到用户数据, 也不做401跳转
 const paths = [
-  /^\/api\/room\/players\/all/,
+  /^\/api\/room\/create/,
   /^\/api\/room\/join/,
   /^\/api\/room\/allPlayers/
 ]
