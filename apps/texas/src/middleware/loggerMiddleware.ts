6921b6b35df81753f68b9a3ec8a5b2d3ac4c1880
yangchengrong <yangchnegrong@yzw.cn>
2025-03-25 13:54:28 +0800

    feat: 记录接口的处理时间

--- apps/texas/src/middleware/loggerMiddleware.ts
+++ apps/texas/src/middleware/loggerMiddleware.ts
@@ -6,11 +6,16 @@ const loggerMiddleware = async (ctx: Context, next: () => Promise<void>) => {
   const { url } = ctx.request
   const ip = ctx.headers['x-real-ip'] || ctx.request.ip
 
+  // 记录开始时间
+  const start = Date.now()
+  await next()
+
+  // 计算响应时间
+  const ms = Date.now() - start
   logger.info(
     `ip: ${ip}, request for ${url}, body: ${JSON.stringify(
       ctx.request.body || {}
-    )}`
+    )}; 耗时${ms}ms`
   )
-  await next()
 }
 export default loggerMiddleware
