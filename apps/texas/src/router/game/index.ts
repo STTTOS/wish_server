9b4d5a3357430f618802bba5b477c762bc411112
yangchengrong <yangchnegrong@yzw.cn>
2025-04-14 10:30:35 +0800

    fix: 删除不必要的日志记录

--- apps/texas/src/router/game/index.ts
+++ apps/texas/src/router/game/index.ts
@@ -39,9 +39,6 @@ router.post(toolsApi('/start/:roomId'), async (ctx) => {
       // 掉线后需要向其他玩家推送当前玩家的状态信息
       // 同时需要将Player的状态置为offline
 
-      logger.info(
-        `ws值: ${JSON.stringify({ userId, restrict, allowedActions })}`
-      )
       // // 向其他玩家推送当前正在行动的玩家
       logger.info('向客户端推送player-action事件')
       ws.broadcastTo(userId, {
