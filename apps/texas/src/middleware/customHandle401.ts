dda414222a45c76db58f8353313af239dcc6b2c4
STTTOS <744765302@qq.com>
2026-03-20 12:46:23 +0800

    fix: 删除无用代码

--- apps/texas/src/middleware/customHandle401.ts
+++ apps/texas/src/middleware/customHandle401.ts
@@ -4,8 +4,6 @@ import { ParameterizedContext } from 'koa'
 import response from '../utils/response'
 import { DefaultState } from '../router/instance'
 
-const loginUsers = new Map<number, { sessionId: string; time: string }>()
-
 // 如下的接口, 即使解析不到用户数据, 也不做401跳转
 const paths = [
   /^\/api\/client\/user\/sign$/,
@@ -37,17 +35,6 @@ const customHandle401 = async (
     response.success(ctx, null, '身份凭证无效, 请重新登陆', 401)
     return
   }
-
-  const data = loginUsers.get(user.id)
-  if (data && data.sessionId !== user?.sessionId) {
-    response.success(
-      ctx,
-      null,
-      `你的账号于${data.time}在其他设备登录, 请重新登陆`,
-      401
-    )
-  } else {
-    await next()
-  }
+  await next()
 }
 export default customHandle401
