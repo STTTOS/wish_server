ecd7395b75da0946502d201ac950a0c3303f3c9f
STTTOS <744765302@qq.com>
2026-04-09 13:56:22 +0800

    fix: 删除重复的Migration

--- apps/texas/prisma/migrations/20260408174216_modifiy/migration.sql
+++ apps/texas/prisma/migrations/20260408174216_modifiy/migration.sql
@@ -1,5 +1,3 @@
 -- AlterTable
 ALTER TABLE `PlayerMatchRecord` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
 
--- AlterTable
-ALTER TABLE `Room` ADD COLUMN `gameStatus` ENUM('waiting', 'entering', 'in_hand', 'between_hands') NOT NULL DEFAULT 'waiting';
